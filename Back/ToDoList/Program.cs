64e643f7b2f9a8f3b26b28eb73716be16afbf0cf
Gustavo Macario <gustavomacario003@gmail.com>
2024-04-22 10:52:06 -0300

    add swagger comments

--- Back/ToDoList/Program.cs
+++ Back/ToDoList/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using ToDoList.Context;
 using ToDoList.Interfaces.Repositories;
 using ToDoList.Interfaces.Services;
@@ -12,7 +14,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Todo_List_API",
+        Version = "v1",
+        Description = ""
+    });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
--- Back/ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ Back/ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d75e166de69e981bfb6aeca78c7778d07fc4188")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
